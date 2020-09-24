61ea229045bb137355a6992cba5c5e6c1e49ff09
Slash.Huang <slash.huang@dfi.com>
2020-09-24 15:53:22 +0800

    move CONFIG_FEC_MXC to mx6qsabresd_defconfig

--- include/configs/mx6sabre_common.h
+++ include/configs/mx6sabre_common.h
@@ -23,7 +23,7 @@
 /* MMC Configs */
 #define CONFIG_SYS_FSL_ESDHC_ADDR      0
 
-#define CONFIG_FEC_MXC
+/* #define CONFIG_FEC_MXC */
 #define CONFIG_MII
 #define IMX_FEC_BASE			ENET_BASE_ADDR
 #define CONFIG_FEC_XCV_TYPE		RGMII
